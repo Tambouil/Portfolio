b4c8e4e4f19aa7425b5a19435b6b82b44a3ca8f8
Valentin Berceaux <v.berceaux@gmail.com>
2022-10-25 09:45:43 +0200

    chore: tailwind automatic dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class",
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
         dark: {
-          50: "#EDEDED",
-          100: "#dadada",
-          200: "#b5b5b5",
-          300: "#8f8f8f",
-          400: "#6a6a6a",
-          500: "#454545",
-          600: "#373737",
-          700: "#292929",
-          800: "#1c1c1c",
-          850: "#171717",
-          900: "#0e0e0e",
+          50: '#EDEDED',
+          100: '#dadada',
+          200: '#b5b5b5',
+          300: '#8f8f8f',
+          400: '#6a6a6a',
+          500: '#454545',
+          600: '#373737',
+          700: '#292929',
+          800: '#1c1c1c',
+          850: '#171717',
+          900: '#0e0e0e',
         },
       },
       container: {
         center: true,
-        padding: "1rem",
+        padding: '1rem',
       },
       backgroundImage: {
-        "dark-hero": "url('/assets/dark-hero.png')",
-        "light-hero": "url('/assets/light-hero.png')",
+        'dark-hero': "url('/assets/dark-hero.png')",
+        'light-hero': "url('/assets/light-hero.png')",
       },
       screens: {
-        "2xl": "1440px",
+        '2xl': '1440px',
       },
     },
   },
