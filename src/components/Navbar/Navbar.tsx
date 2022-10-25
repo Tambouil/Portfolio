09c66a9badfb3231d3128bb4d26e87b8ac276f54
Valentin Berceaux <v.berceaux@gmail.com>
2022-10-25 13:41:14 +0200

    refactor(navbar): add use-scrollpy for desktop devices

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,32 +1,42 @@
 import { motion } from 'framer-motion';
+import { useScrollspy } from 'use-scrollspy';
 import { linkVariants, navVariants } from './NavbarMotion';
 
-const Navbar = () => (
-  <motion.header
-    variants={navVariants}
-    initial="hidden"
-    animate="visible"
-    className="container fixed inset-x-0 top-0 z-50 hidden h-32 w-full items-center justify-between md:flex"
-  >
-    <motion.a className="nav-link" href="#" variants={linkVariants}>
-      VB.
-    </motion.a>
+const Navbar = () => {
+  useScrollspy({
+    ids: ['intro', 'projects', 'about', 'contact'],
+    hrefs: ['/#intro', '/#projects', '/#about', '/#contact'],
+    offset: 'topCenter',
+    activeClass: 'active-nav-link',
+  });
 
-    <nav className="flex items-center justify-center gap-x-14 text-lg">
-      <motion.a className="nav-link" href="#intro" variants={linkVariants}>
-        Introduction
+  return (
+    <motion.header
+      variants={navVariants}
+      initial="hidden"
+      animate="visible"
+      className="container fixed inset-x-0 top-0 z-50 hidden h-32 w-full items-center justify-between md:flex"
+    >
+      <motion.a className="nav-link" href="/" variants={linkVariants}>
+        VB.
       </motion.a>
-      <motion.a className="nav-link" href="#projects" variants={linkVariants}>
-        Projects
-      </motion.a>
-      <motion.a className="nav-link" href="#about" variants={linkVariants}>
-        About
-      </motion.a>
-      <motion.a className="nav-link" href="#contact" variants={linkVariants}>
-        Contact
-      </motion.a>
-    </nav>
-  </motion.header>
-);
+
+      <nav className="flex items-center justify-center gap-x-14 text-lg">
+        <motion.a className="nav-link" href="/#intro" variants={linkVariants}>
+          Introduction
+        </motion.a>
+        <motion.a className="nav-link" href="/#projects" variants={linkVariants}>
+          Projects
+        </motion.a>
+        <motion.a className="nav-link" href="/#about" variants={linkVariants}>
+          About
+        </motion.a>
+        <motion.a className="nav-link" href="/#contact" variants={linkVariants}>
+          Contact
+        </motion.a>
+      </nav>
+    </motion.header>
+  );
+};
 
 export default Navbar;
