e10a4a7cc8ef7e8866253b3a126f2f115df577e5
Valentin Berceaux <v.berceaux@gmail.com>
2022-10-26 13:02:07 +0200

    refactor(favicon): replace favicon with default vercel svg

--- src/layouts/Meta.tsx
+++ src/layouts/Meta.tsx
@@ -1,4 +1,4 @@
-import Head from "next/head";
+import Head from 'next/head';
 
 interface MetaProps {
   title: string;
@@ -8,26 +8,19 @@ interface MetaProps {
 
 const Meta = ({ title, keywords, description }: MetaProps) => {
   return (
-
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="keywords" content={keywords} />
       <meta name="description" content={description} />
       <meta charSet="utf-8" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.svg" />
       <title>{title}</title>
       <meta name="robots" content="index,follow" />
       <meta property="og:site_name" content="Portfolio" />
-      <meta
-        property="og:title"
-        content="Valentin Berceaux | Full Stack Developer"
-      />
+      <meta property="og:title" content="Valentin Berceaux | Full Stack Developer" />
       <meta
         name="description"
         content="Bonjour 👋, I'm Valentin Berceaux, a fullstack web developer with Back-End affinity from West of France."
-
-
-
       />
       <meta
         property="og:image"
@@ -39,9 +32,8 @@ const Meta = ({ title, keywords, description }: MetaProps) => {
 };
 
 Meta.defaultProps = {
-  title: "Valentin Berceaux | Full Stack Developer",
-  keywords:
-    "next.js, react, web, ui, freelance, dev, development, node, node js",
+  title: 'Valentin Berceaux | Full Stack Developer',
+  keywords: 'next.js, react, web, ui, freelance, dev, development, node, node js',
   description:
     "Bonjour 👋, I'm Valentin Berceaux, a fullstack web developer with Back-End affinity from West of France.",
 };
